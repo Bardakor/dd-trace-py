90c4278d0a4acd70f88bdf86c14b8aff02edeede
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2026-07-20 17:31:14 +0200

    chore(profiling): add support for reporting telemetry errors

--- ddtrace/internal/datadog/profiling/stack/src/stack.cpp
+++ ddtrace/internal/datadog/profiling/stack/src/stack.cpp
@@ -894,6 +894,16 @@ stack_is_safe_copy_failed(PyObject* Py_UNUSED(self), PyObject* Py_UNUSED(args))
     Py_RETURN_FALSE;
 }
 
+static PyObject*
+stack_take_sampling_thread_error(PyObject* Py_UNUSED(self), PyObject* Py_UNUSED(args))
+{
+    auto error = Sampler::get().take_sampling_thread_error();
+    if (!error.has_value()) {
+        Py_RETURN_NONE;
+    }
+    return Py_BuildValue("(ss)", error->type_name.c_str(), error->message.c_str());
+}
+
 static PyMethodDef stack_methods[] = {
     { "start", reinterpret_cast<PyCFunction>(stack_start), METH_VARARGS | METH_KEYWORDS, "Start the sampler" },
     { "stop", stack_stop, METH_VARARGS, "Stop the sampler" },
@@ -956,6 +966,10 @@ static PyMethodDef stack_methods[] = {
       stack_is_safe_copy_failed,
       METH_NOARGS,
       "Check if all safe copy methods failed to initialize" },
+    { "take_sampling_thread_error",
+      stack_take_sampling_thread_error,
+      METH_NOARGS,
+      "Return and clear the (error_type, message) that terminated the sampling thread, or None" },
     { "uninstall_segv_handler",
       stack_uninstall_segv_handler,
       METH_NOARGS,
